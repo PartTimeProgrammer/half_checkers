fa196469ed5f833f0fc0d5e9112b07329b35170e
PartTimeProgrammer <jacetothev@yahoo.com>
2012-09-02 10:02:44 -0700

    Update RunCheckers.java

--- RunCheckers.java
+++ RunCheckers.java
@@ -6,7 +6,7 @@
   This is the game of checkers.  Right now the game enforces some rules
   but relies on the users to enforce some as well.
   
-  For instance I was unable to force the one tile move. Also the logice to force a jump
+  For instance I was unable to force the one tile move. Also the logic to force a jump
   is not correct either.  There is no automated win condition either.
   
   This is the most complex program I have written, and I dont think it messaures up.
